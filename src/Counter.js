7c7fe15613510bfa4b01b0d9078f83761d65c9ca
Muhammad <ranaahmad131@gmail.com>
2023-09-19 16:43:51 +0500

    Working-fine

---
+++ src/Counter.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(5);
+
+  const handleDecrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const date = new Date("June 21, 2027");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <button onClick={handleDecrement}>-</button>
+      <span>
+        {date.toDateString()} [{count}]
+      </span>
+      <button onClick={handleIncrement}>+</button>
+    </div>
+  );
+}
+
+export default Counter;
